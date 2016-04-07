c00bfdf20aad3ecb278540fd73d4b6cba553a8d7
samshareski <samshareski@gmail.com>
2016-04-06 23:13:06 -0400

    Add probablistic ai

--- hanabi.py
+++ hanabi.py
@@ -186,6 +186,145 @@ class SimpleHeuristic(Heuristic):
         return best_move
 
 
+class ProbabilisticHeurstic(Heuristic):
+    def __init__(self, player):
+        super(ProbabilisticHeurstic, self).__init__(player)
+        self.threshold = 0.75
+        self.sudden_death_threshold = 0.90
+        self.whole_deck = [Card(Colour.white, 1),
+                           Card(Colour.white, 1),
+                           Card(Colour.white, 1),
+                           Card(Colour.white, 2),
+                           Card(Colour.white, 2),
+                           Card(Colour.white, 3),
+                           Card(Colour.white, 3),
+                           Card(Colour.white, 4),
+                           Card(Colour.white, 4),
+                           Card(Colour.white, 5),
+                           Card(Colour.yellow, 1),
+                           Card(Colour.yellow, 1),
+                           Card(Colour.yellow, 1),
+                           Card(Colour.yellow, 2),
+                           Card(Colour.yellow, 2),
+                           Card(Colour.yellow, 3),
+                           Card(Colour.yellow, 3),
+                           Card(Colour.yellow, 4),
+                           Card(Colour.yellow, 4),
+                           Card(Colour.yellow, 5),
+                           Card(Colour.green, 1),
+                           Card(Colour.green, 1),
+                           Card(Colour.green, 1),
+                           Card(Colour.green, 2),
+                           Card(Colour.green, 2),
+                           Card(Colour.green, 3),
+                           Card(Colour.green, 3),
+                           Card(Colour.green, 4),
+                           Card(Colour.green, 4),
+                           Card(Colour.green, 5),
+                           Card(Colour.blue, 1),
+                           Card(Colour.blue, 1),
+                           Card(Colour.blue, 1),
+                           Card(Colour.blue, 2),
+                           Card(Colour.blue, 2),
+                           Card(Colour.blue, 3),
+                           Card(Colour.blue, 3),
+                           Card(Colour.blue, 4),
+                           Card(Colour.blue, 4),
+                           Card(Colour.blue, 5),
+                           Card(Colour.red, 1),
+                           Card(Colour.red, 1),
+                           Card(Colour.red, 1),
+                           Card(Colour.red, 2),
+                           Card(Colour.red, 2),
+                           Card(Colour.red, 3),
+                           Card(Colour.red, 3),
+                           Card(Colour.red, 4),
+                           Card(Colour.red, 4),
+                           Card(Colour.red, 5)]
+
+    def best_move(self):
+        player_hand = self.player.hand
+        oldest = 50
+        for i, card_in_hand in enumerate(player_hand):
+            if card_in_hand.timestamp < oldest:
+                oldest = card_in_hand.timestamp
+                best_move = Move(self.player.discard, i)
+
+        playable_cards = self.player.game.play_area.playable_cards()
+        if self.player.game.time > 0:
+            playable_info = {Colour.white: 0,
+                             Colour.yellow: 0,
+                             Colour.green: 0,
+                             Colour.blue: 0,
+                             Colour.red: 0,
+                             1: 0,
+                             2: 0,
+                             3: 0,
+                             4: 0,
+                             5: 0}
+            partner_hand = self.player.partner.hand
+            for card_in_hand in partner_hand:
+                if not card_in_hand.colour:
+                    if card_in_hand.card in playable_cards:
+                        playable_info[card_in_hand.card.colour] += 2
+                    else:
+                        playable_info[card_in_hand.card.colour] += 1
+                if not card_in_hand.number:
+                    if card_in_hand.card in playable_cards:
+                        playable_info[card_in_hand.card.number] += 2
+                    else:
+                        playable_info[card_in_hand.card.number] += 1
+
+            best_value = 0
+            for info, value in playable_info.items():
+                if value > best_value:
+                    best_value = value
+                    best_move = Move(self.player.give_info, info)
+
+        if self.player.game.fuse > 1:
+            threshold = self.threshold
+        else:
+            threshold = self.sudden_death_threshold
+        best_prob = 0
+        for i, card_in_hand in enumerate(self.player.hand):
+            prob = self.calc_percentages(card_in_hand, self.player.hand, self.player.partner.hand,
+                                         self.player.game.play_area, self.player.game.discard_pile)
+            if prob > threshold and prob > best_prob:
+                best_move = Move(self.player.play, i)
+
+        return best_move
+
+    def calc_percentages(self, given_card_in_hand, player_hand, partner_hand, play_area, discard_pile):
+        possible_cards = list(self.whole_deck)
+        for card_in_hand in partner_hand:
+            possible_cards.remove(card_in_hand.card)
+        for card_in_hand in player_hand:
+            if not card_in_hand == given_card_in_hand and card_in_hand.colour and card_in_hand.number:
+                possible_cards.remove(card_in_hand.card)
+        for card in discard_pile:
+            possible_cards.remove(card)
+        for suit in play_area.played.values():
+            for card in suit:
+                possible_cards.remove(card)
+        possible_cards = list(filter(lambda card: card.colour in given_card_in_hand.possible_colours,
+                                     possible_cards))
+        possible_cards = list(filter(lambda card: card.number in given_card_in_hand.possible_numbers,
+                                     possible_cards))
+        n = len(possible_cards)
+        playable_cards = play_area.playable_cards()
+        playable_probability = 0
+        for card in possible_cards:
+            if card in playable_cards:
+                playable_probability += 1 / n
+
+        return playable_probability
+
+
+
+
+
+
+
 class Move:
     def __init__(self, move, arg):
         self.move = move
@@ -348,11 +487,16 @@ class Deck:
 
 if __name__ == '__main__':
     total = 0
-    n_games = 10000
+    games_failed = 0
+    n_games = 100
     for _ in range(n_games):
-        game = Hanabi(SimpleHeuristic)
+        game = Hanabi(ProbabilisticHeurstic)
         score, fail = game.play_game()
-        total += score
-    print(total / n_games)
+        if fail:
+            games_failed += 1
+        else:
+            total += score
+    print(games_failed / n_games)
+    print(total / (n_games - games_failed))
 
 
